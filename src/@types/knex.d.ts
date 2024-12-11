44b5f0ba76f15eb037ff92ceab16ea4d9ed4fc90
murilodamarioo <murilo.gcdamario@gmail.com>
2024-12-10 22:03:19 -0300

    feat: implement table types for users table

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,13 @@
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    users: {
+      id: string
+      session_id: string
+      name: string
+      email: string
+      created_at: string
+    }
+  }
+}
