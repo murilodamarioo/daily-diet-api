4680cf7e153e5f01aa313c50c6e47f6988aa81d3
murilodamarioo <murilo.gcdamario@gmail.com>
2024-12-10 22:46:58 -0300

    feat: implement middleware to check if cookie exists

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import fastify from 'fastify'
-import { knex } from './database';
 import { env } from './env';
+import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 import { usersRoutes } from './routes/users';
 
 const app = fastify();
+
+app.register(cookie)
 
 app.register(usersRoutes, {
   prefix: '/users'
