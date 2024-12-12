fc52d87fc00b8204c80dd490e604bba8e3e26bb4
murilodamarioo <murilo.gcdamario@gmail.com>
2024-12-12 15:25:54 -0300

    fix: database tables

--- db/migrations/20241212181811_create-users.ts
+++ db/migrations/20241212181811_create-users.ts
@@ -3,11 +3,11 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('users', (table) => {
-    table.uuid('id').primary,
+    table.uuid('id').primary(),
     table.string('session_id').notNullable().unique()
     table.string('name').notNullable()
     table.string('email').notNullable().unique()
-    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    table.timestamps(true, true)
   })
 }
 
--- db/migrations/20241212181844_create-meals.ts
+++ db/migrations/20241212181844_create-meals.ts
@@ -4,12 +4,12 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('meals', (table) => {
     table.uuid('id').primary()
-    table.uuid('user_id').references('users.id').notNullable()
+    table.uuid('user_id').references('id').inTable('users').onDelete('CASCADE').notNullable()
     table.string('name').notNullable()
     table.string('description').notNullable()
     table.boolean('is_on_diet').notNullable()
     table.date('date').notNullable()
-    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    table.timestamps(true, true)
   })
 }
 
