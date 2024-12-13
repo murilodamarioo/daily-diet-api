e49237da99e66721694a5974a7616076bb4fee98
murilodamarioo <murilo.gcdamario@gmail.com>
2024-12-13 10:03:51 -0300

    refactor: change date type

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -16,7 +16,7 @@ declare module 'knex/types/tables' {
       name: string,
       description: string,
       is_on_diet: boolean,
-      date: string,
+      date: number,
       created_at: string,
       updated_at: string
     }
