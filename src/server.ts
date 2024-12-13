8bce06d060a966ef742e7ed074250ffd2b93d1c6
murilodamarioo <murilo.gcdamario@gmail.com>
2024-12-13 15:45:30 -0300

    refactor: move register routes to app file

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,6 @@
 import { env } from './env';
-import fastify from 'fastify'
-import cookie from '@fastify/cookie'
-import { usersRoutes } from './routes/users';
-import { mealsRoutes } from './routes/meals';
+import { app } from './app';
 
-const app = fastify();
-
-app.register(cookie)
-
-app.register(usersRoutes, {
-  prefix: '/users'
-})
-
-app.register(mealsRoutes, {
-  prefix: '/meals'
-})
 
 app.listen({
   port: env.PORT,
