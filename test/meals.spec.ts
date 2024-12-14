cbbfa7f9377cb830b33de7a4a5cbf8af63059223
murilodamarioo <murilo.gcdamario@gmail.com>
2024-12-14 10:36:21 -0300

    feat: implement get by id and put routes tests

--- test/meals.spec.ts
+++ test/meals.spec.ts
@@ -2,8 +2,6 @@ import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
 import { execSync } from 'child_process'
 import { app } from '../src/app'
 import request from 'supertest'
-import { date } from 'zod'
-import knex from 'knex'
 
 
 describe('Meals routes', () => {
@@ -23,63 +21,146 @@ describe('Meals routes', () => {
     execSync('npm run knex migrate:latest')
   })
 
-  it('should be able to create a new meal', async () => {
-    const userResponse = await request(app.server).post('/users').send({
-      name: 'John Doe',
-      email: 'john@email.com'
-    }).expect(201)
+  describe('POST - success cases', () => {
+    it('should be able to create a new meal', async () => {
+      const userResponse = await request(app.server).post('/users').send({
+        name: 'John Doe',
+        email: 'john@email.com'
+      }).expect(201)
 
-    const cookies = userResponse.get('Set-Cookie');
-    expect(cookies).toBeDefined();
+      const cookies = userResponse.get('Set-Cookie');
+      expect(cookies).toBeDefined();
 
-    await request(app.server).post('/meals')
-    .set('Cookie', cookies)
-    .send({
-      name: 'Breakfast',
-      description: "It's a breakfast",
-      isOnDiet: true,
-      date: new Date(),
+      await request(app.server).post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Breakfast',
+        description: "It's a breakfast",
+        isOnDiet: true,
+        date: new Date(),
+      })
+      .expect(201)
     })
-    .expect(201)
   })
+  
+  describe('GET - success cases', () => {
+    it('should be able to list all meals', async () => {
+      const userResponse = await request(app.server).post('/users').send({
+        name: 'John Doe',
+        email: 'john@email.com'
+      }).expect(201)
 
-  it('should be able to list all meals', async () => {
-    const userResponse = await request(app.server).post('/users').send({
-      name: 'John Doe',
-      email: 'john@email.com'
-    }).expect(201)
+      const cookies = userResponse.get('Set-Cookie');
+      expect(cookies).toBeDefined();
 
-
-    const cookies = userResponse.get('Set-Cookie');
-    expect(cookies).toBeDefined();
-
-    await request(app.server)
-    .post('/meals')
-    .set('Cookie', cookies)
-    .send({
-      name: 'Breakfast',
-      description: "It's a breakfast",
-      isOnDiet: true,
-      date: new Date(),
-    })
-    .expect(201)
-
-    await request(app.server)
+      await request(app.server)
       .post('/meals')
       .set('Cookie', cookies)
       .send({
-        name: 'Lunch',
-        description: "It's a lunch",
+        name: 'Breakfast',
+        description: "It's a breakfast",
         isOnDiet: true,
-        date: new Date(Date.now() + 24 * 60 * 60 * 1000), // 1 day after
+        date: new Date(),
       })
       .expect(201)
-    
-    const mealsResponse = await request(app.server)
-    .get('/meals')
-    .set('Cookie', cookies)
-    .expect(200)
 
-    expect(mealsResponse.body.meals).toHaveLength(2)
+      await request(app.server)
+        .post('/meals')
+        .set('Cookie', cookies)
+        .send({
+          name: 'Lunch',
+          description: "It's a lunch",
+          isOnDiet: true,
+          date: new Date(Date.now() + 24 * 60 * 60 * 1000), // 1 day after
+        })
+        .expect(201)
+      
+      const mealsResponse = await request(app.server)
+      .get('/meals')
+      .set('Cookie', cookies)
+      .expect(200)
+
+      expect(mealsResponse.body.meals).toHaveLength(2)
+    })
+
+    it('should be able to show a single meal', async () => {
+      const userResponse = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'John Doe',
+        email: 'john@email.com'
+      })
+      .expect(201)
+
+      const cookies = userResponse.get('Set-Cookie')
+      expect(cookies).toBeDefined()
+
+      await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Breakfast',
+        description: "It's a breakfast",
+        isOnDiet: true,
+        date: new Date(),
+      })
+      .expect(201)
+      
+      const mealsResponse = await request(app.server)
+      .get('/meals')
+      .set('Cookie', cookies)
+      .expect(200)
+
+      const mealId = mealsResponse.body.meals[0].id
+
+      await request(app.server)
+        .get(`/meals/${mealId}`)
+        .set('Cookie', cookies)
+        .expect(200)
+    })
+  })
+  
+  describe('PUT - success cases', () => {
+    it('shoud be able to update a meal', async () => {
+      const userResponse = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'John Doe',
+        email: 'john@email.com'
+      })
+      .expect(201)
+
+      const cookies = userResponse.get('Set-Cookie')
+      expect(cookies).toBeDefined()
+
+      await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Breakfast',
+        description: "It's a breakfast",
+        isOnDiet: true,
+        date: new Date(),
+      })
+      .expect(201)
+
+      const mealsResponse = await request(app.server)
+      .get('/meals')
+      .set('Cookie', cookies)
+      .expect(200)
+
+      const mealId = mealsResponse.body.meals[0].id
+
+      await request(app.server)
+      .put(`/meals/${mealId}`)
+      .set('Cookie', cookies)
+      .send({
+        name: 'Dinne',
+        description: "It's a dinner",
+        isOnDiet: true,
+        date: new Date()
+      })
+      .expect(204)
+    })
   })
 })
