fe8960c31e2d62205d0003fcb63b8cb8456d13ba
murilodamarioo <murilo.gcdamario@gmail.com>
2024-12-15 16:17:30 -0300

    feat: should be able to show metrics from a user

--- test/meals.spec.ts
+++ test/meals.spec.ts
@@ -118,10 +118,81 @@ describe('Meals routes', () => {
         .set('Cookie', cookies)
         .expect(200)
     })
+
+    
+    it('should be able to show metrics from a user', async () => {
+      const userResponse = await request(app.server)
+        .post('/users')
+        .send({
+          name: 'John Doe',
+          email: 'john@email.com'
+        })
+        .expect(201)
+
+      const cookies = userResponse.get('Set-Cookie')
+      expect(cookies).toBeDefined()
+
+      await request(app.server)
+        .post('/meals')
+        .set('Cookie', cookies)
+        .send({
+          name: 'Breakfast',
+          description: "It's a breakfast",
+          isOnDiet: true,
+          date: new Date('2021-01-01T08:00:00'),
+        })
+        .expect(201)
+
+      await request(app.server)
+        .post('/meals')
+        .set('Cookie', cookies)
+        .send({
+          name: 'Lunch',
+          description: "It's a lunch",
+          isOnDiet: true,
+          date: new Date('2021-01-01T12:00:00'),
+        })
+        .expect(201)
+
+
+        await request(app.server)
+        .post('/meals')
+        .set('Cookie', cookies)
+        .send({
+          name: 'Cheese Cake',
+          description: "It's a cheese cake",
+          isOnDiet: false,
+          date: new Date('2021-01-01T14:00:00'),
+        })
+        .expect(201)
+
+        await request(app.server)
+        .post('/meals')
+        .set('Cookie', cookies)
+        .send({
+          name: 'Dinner',
+          description: "It's a dinner",
+          isOnDiet: true,
+          date: new Date('2021-01-01T19:00:00'),
+        })
+        .expect(201)
+
+      const metricsResponse = await request(app.server)
+        .get('/meals/metrics')
+        .set('Cookie', cookies)
+        .expect(200)
+
+      expect(metricsResponse.body.metrics).toEqual({
+        bestOnDietSequence: 2,
+        meals: 4,
+        mealsOnDiet: 3,
+        mealsOffDiet: 1
+      })
+    })
   })
   
   describe('PUT - success cases', () => {
-    it('shoud be able to update a meal', async () => {
+    it('should be able to update a meal', async () => {
       const userResponse = await request(app.server)
       .post('/users')
       .send({
